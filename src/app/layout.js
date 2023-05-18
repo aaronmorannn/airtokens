989557427b0b3628ea75b91ecf030ec4a1f2784a
Aaron Moran <aaron@Aarons-MacBook-Air.local>
2023-05-18 17:34:46 +0100

    google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,6 +12,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-2V69D91S50"></Script>
+      <Script>
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'G-2V69D91S50');
+      `}
+      </Script>
       <body className={inter.className}>{children}</body>
     </html>
   )

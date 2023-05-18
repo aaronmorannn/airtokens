760ba789135a7270cb194525250eb4ce7ed11c4b
Aaron Moran <aaron@Aarons-MacBook-Air.local>
2023-05-18 18:15:01 +0100

    ga

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-// import Script from 'next/script'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,16 +12,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* <Script async src="https://www.googletagmanager.com/gtag/js?id=G-2V69D91S50"></Script>
-      <Script>
+    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-2V69D91S50"></Script>
+    <Script>
       {`
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-      
+  
         gtag('config', 'G-2V69D91S50');
       `}
-      </Script> */}
+    </Script>
       <body className={inter.className}>{children}</body>
     </html>
   )

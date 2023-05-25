46302ff88f86aa062fd5998eb5a72175ccb223dd
Aaron Moran <aaron@Aarons-MacBook-Air.local>
2023-05-25 22:52:29 +0100

    meta

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,8 +6,8 @@ import Head from 'next/head';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Lightweight Crypto Chart',
-  description: 'Web Application that is tracking the Top 100 Cryptocurrencies, refreshing every 10 seconds.',
+  title: 'Air Tokens - Cryptocurrency Charts, Prices and Rankings',
+  description: 'Super lightweight cryptocurrency chart that is monitoring the rankings and prices of the top 100 coins, updating every 10 seconds.',
 }
 
 export default function RootLayout({ children }) {
@@ -15,9 +15,10 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Head>
         <meta charset="UTF-8"/>
-        <meta name="keywords" content="Cryptocurrency, Crypto Charts, Bitcoin, Ethereum, Dogecoin, Satoshi Nakamoto, Crypto News, Coin, Wallet, Blockchain, Token, Exchange"/>
+        <meta name="keywords" content="Cryptocurrency, Crypto Charts, Bitcoin, Ethereum, Dogecoin, Satoshi Nakamoto, Crypto News, Rankings, Prices, Coin, Wallet, Blockchain, Token, Exchange"/>
         <meta name="auto" content="Air Tokens"/>
         <meta name="robots" content="noindex"/>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico"/>
       </Head>
       <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-2V69D91S50"

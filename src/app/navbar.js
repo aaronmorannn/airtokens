3799bcddb8256addae908e16f3705445e64f9566
aaronmorannn <aaronmorann@icloud.com>
2023-08-08 21:51:36 +0100

    coins

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -1,28 +1,21 @@
 "use client"
 import { Disclosure } from '@headlessui/react'
 
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
-
 export default function Example() {
   return (
-    <Disclosure as="nav" className="sticky top-0 bg-white p-1">
+    <Disclosure as="nav" className="sticky top-0 bg-white p-1 bg-opacity-80 backdrop-blur-lg backdrop-filter border-b">
       {({ open }) => (
         <>
-          <div className=" mx-auto pb-4 ">
-            <div className=" flex h-4 pt-2 items-center justify-between">
-              <div className="flex flex-1 items-center justify-center ">
-                <div className='max-sm:pb-2 pt-4 flex '>
+          <div className=" mx-auto pb-7">
+            <div className=" flex h-4 pt-2">
+              <div className="flex flex-1">
+                <div className='max-sm:pb-2 pl-4 flex '>
                   <a href="/"><span class="relative flex h-5 w-5">
                     <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-500 opacity-75"></span>
                     <span class="relative inline-flex rounded-full h-5 w-5 bg-green-500"></span>
                   </span></a>
-                  <p className='align-middle pl-4 text-sm text-slate-500'>AirTokens</p>
                 </div>
               </div>
-
             </div>
           </div>
 
--- src/app/page.js
+++ src/app/page.js
@@ -52,16 +52,15 @@ export default class Home extends React.Component {
       });
 
     return (
-      <main className='flex justify-center items-center pb-2 max-sm:p-4 pt-6'>
+      <main className='flex p-4 pb-2 max-sm:p-4 pt-6'>
         <table class="table-auto md:table-fixed max-sm:table-auto max-sm:text-sm">
           <tbody>
             {
               this.state.dataSource.map((val,key) =>{
                 return <tr className='' key={key}>
-                  <td className='font-medium text-sm '><a href={`http://www.google.com/search?q=${val.name} cryptocurrency`} className='hover:text-blue-500 hover:underline hover:underline-offset-4'>{val.name}</a></td>
-                  <td className='text-[#6c6c70] text-sm font-light pl-4 '>({val.symbol}) </td>
-                  <td className='font-normal text-sm align-middle'>${val.price_usd}</td> 
-                  <td className='pl-4 p-2 text-sm align-middle'>{val.percent_change_24h > 0 ? <span className='text-[#248a3e] '>▲{val.percent_change_24h}%</span> : <span className='text-[#d70015]'>▼{val.percent_change_24h}%</span>}</td>
+                  <td className='font-medium text-sm '><a href={`http://www.google.com/search?q=${val.name} cryptocurrency`} className='hover:text-blue-500 lg:text-6xl md:text-4xl sm:text-2xl max-sm:text-xl hover:underline hover:underline-offset-4'>{val.name}</a></td>
+                  <td className='font-normal lg:text-6xl align-middle md:text-4xl sm:text-2xl max-sm:text-xl'>${val.price_usd}</td> 
+                  <td className='pl-4 p-2 text-sm align-middle md:text-4xl lg:text-6xl sm:text-2xl max-sm:text-xl'>{val.percent_change_24h > 0 ? <span className='text-[#248a3e]'>{val.percent_change_24h}%</span> : <span className='text-[#d70015]'>{val.percent_change_24h}%</span>}</td>
                 </tr>
               })
             }

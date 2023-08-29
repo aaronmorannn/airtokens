0b685ec2b70995c66e5534b7924733aa2ff38f8f
aaronmorannn <aaronmorann@icloud.com>
2023-08-30 00:23:48 +0100

    stuff

--- src/app/page.js
+++ src/app/page.js
@@ -52,14 +52,14 @@ export default class Home extends React.Component {
       });
 
     return (
-      <main className='flex p-4 pb-2 max-sm:p-4 pt-6'>
-        <table class="table-auto md:table-fixed max-sm:table-auto max-sm:text-sm">
+      <main className='flex pb-2'>
+        <table class="table-auto w-full md:table-fixed max-sm:table-auto max-sm:text-sm">
           <tbody>
             {
               this.state.dataSource.map((val,key) =>{
-                return <tr className='' key={key}>
-                  <td className='font-medium text-sm '><a href={`http://www.google.com/search?q=${val.name} cryptocurrency`} className='hover:text-green-500 lg:text-6xl md:text-4xl sm:text-2xl max-sm:text-xl hover:underline hover:underline-offset-4'>{val.name}</a></td>
-                  <td className='font-normal lg:text-6xl align-middle md:text-4xl sm:text-2xl max-sm:text-xl'>${val.price_usd}</td> 
+                return <tr className='border-b hover:bg-gray-100' key={key}>
+                  <td className='font-medium text-sm pl-4'><a href={`http://www.google.com/search?q=${val.name} cryptocurrency`} className='hover:text-green-500 lg:text-6xl md:text-4xl sm:text-2xl max-sm:text-xl'>{val.name}</a></td>
+                  <td className='font-normal lg:text-6xl align-middle md:text-4xl sm:text-2xl max-sm:text-xl pl-4'>${val.price_usd}</td> 
                   <td className='pl-4 p-2 text-sm align-middle md:text-4xl lg:text-6xl sm:text-2xl max-sm:text-xl'>{val.percent_change_24h > 0 ? <span className='text-[#248a3e]'>{val.percent_change_24h}%</span> : <span className='text-[#d70015]'>{val.percent_change_24h}%</span>}</td>
                 </tr>
               })

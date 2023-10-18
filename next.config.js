313c49c8b30edf1175a7e4f1ce681e40dacb461f
Moran98 <aaronmoran2411@gmail.com>
2023-10-18 16:10:15 +0100

    remove pwa

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-// module.exports = nextConfig
-
-const withPWA = require('next-pwa')({
-    dest: 'public'
-  })
-  
-module.exports = withPWA({
-    nextConfig
-})
+module.exports = nextConfig

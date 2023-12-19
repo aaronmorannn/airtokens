4a3b9d9be5bb2aee0b55ff5445982390526a7810
aaronmorannn <aaronmorann@icloud.com>
2023-12-19 13:46:35 +0000

    new column

--- src/app/components/coins.js
+++ src/app/components/coins.js
@@ -31,18 +31,27 @@ export default function GetCoins() {
         placeholder="Search"
         className="p-4 w-full h-20 text-green-500 lg:text-4xl md:text-4xl sm:text-2xl max-sm:text-xl border-b outline-none"
         value={searchTerm}
+        autoFocus
         onChange={(e) => setSearchTerm(e.target.value)}
       />
       <table className="table-auto w-full md:table-fixed max-sm:table-auto max-sm:text-sm">
+        <tbody >
+          <tr className='border-b hover:bg-gray-100'>
+            <td className=' text-sm pl-4 border-r lg:text-2xl md:text-2xl sm:text-xl max-sm:text-xl text-gray-500'>Name</td>
+            <td className=' text-sm pl-4 border-r lg:text-2xl md:text-2xl sm:text-xl max-sm:text-xl text-gray-500'>Price</td>
+            <td className=' text-sm pl-4 border-r lg:text-2xl md:text-2xl sm:text-xl max-sm:text-xl text-gray-500'>1hr</td>
+            <td className=' text-sm pl-4 lg:text-2xl md:text-2xl sm:text-xl max-sm:text-xl text-gray-500'>24hr</td>
+          </tr>
         {(searchTerm ? searchResults : data).map((val) => (
-          <tbody key={val.id}>
-            <tr className='border-b hover:bg-gray-100' >
+            <tr key={val.id} className='border-b hover:bg-gray-100' >
                 <td className=' text-sm pl-4 border-r'><a href={`http://www.google.com/search?q=${val.name} cryptocurrency`} className='hover:text-green-500 lg:text-4xl md:text-4xl sm:text-2xl max-sm:text-xl'>{val.name}</a></td>
                 <td className='text-black lg:text-4xl align-middle border-r md:text-4xl sm:text-2xl max-sm:text-xl pl-4'>${val.price_usd}</td> 
+                <td className='pl-4 p-2 border-r text-sm align-middle md:text-4xl lg:text-4xl sm:text-2xl max-sm:text-xl'>{val.percent_change_1h > 0 ? <span className='text-[#248a3e]'>{val.percent_change_1h}%</span> : <span className='text-[#d70015]'>{val.percent_change_1h}%</span>}</td>
                 <td className='pl-4 p-2 text-sm align-middle md:text-4xl lg:text-4xl sm:text-2xl max-sm:text-xl'>{val.percent_change_24h > 0 ? <span className='text-[#248a3e]'>{val.percent_change_24h}%</span> : <span className='text-[#d70015]'>{val.percent_change_24h}%</span>}</td>
             </tr>
-          </tbody>
         ))}
+        </tbody>
+
       </table>
     </main>
   );
